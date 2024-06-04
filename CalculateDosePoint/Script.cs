288a9a264beac2e91beff2cad61e71e35633d1e2
Erik Fura <100349708+FlygandeFaran@users.noreply.github.com>
2024-06-04 10:43:38 +0200

    Add files via upload
    First commit

---
+++ CalculateDosePoint/Script.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using System.Windows.Media.Media3D;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context)
+        {
+            run(context.IonPlanSetup, context.Patient);
+        }
+        public void run(IonPlanSetup plan, Patient patient)
+        {
+            CSI_GUI.MainForm form = new CSI_GUI.MainForm(plan);
+            form.ShowDialog();
+        }
+    }
+}
+
